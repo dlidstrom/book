2383a3f443e47caa3c934947bd113a1e320cd73c
Daniel Lidstrom <dlidstrom@gmail.com>
2010-11-09 20:49:35 +0100

    Can now read number of records.

--- lib/book.rb
+++ lib/book.rb
@@ -2,8 +2,10 @@ class Book
   def initialize(file)
     open file, 'rb' do |f|
       @version = f.read(4).unpack('L').shift
+      @records = f.read(4).unpack('L').shift
     end
   end
 
   attr_accessor :version
+  attr_accessor :records
 end
--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -2,9 +2,16 @@ require 'book'
 
 describe Book do
   describe "loading" do
+    before(:each) do
+      @book = Book.new "spec/data/JA_s12.book"
+    end
+
     it "should read version" do
-      book = Book.new "spec/data/JA_s12.book"
-      book.version.should == 1
+      @book.version.should == 1
+    end
+
+    it "should read number of records" do
+      @book.records.should == 91
     end
   end
 end
