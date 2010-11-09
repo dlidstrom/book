ac7c9c5ed26bcf52a073ef69e054475a76591f18
Daniel Lidstrom <dlidstrom@gmail.com>
2010-11-09 20:46:13 +0100

    Reorganized slightly.

--- lib/book.rb
+++ lib/book.rb
@@ -1,12 +1,9 @@
 class Book
-  
   def initialize(file)
     open file, 'rb' do |f|
       @version = f.read(4).unpack('L').shift
     end
   end
-  
-  def version
-    @version
-  end
+
+  attr_accessor :version
 end
--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,8 +1,10 @@
 require 'book'
 
 describe Book do
-  it "should read version" do
-    book = Book.new "spec/data/JA_s12.book"
-    book.version.should == 1
+  describe "loading" do
+    it "should read version" do
+      book = Book.new "spec/data/JA_s12.book"
+      book.version.should == 1
+    end
   end
 end
